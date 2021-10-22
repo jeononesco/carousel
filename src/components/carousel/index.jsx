e03e5f8b93f4e2e6794c0294f462b6d0068f2dc6
Jan Nonesco <jan_nonesco@medilink.com.ph>
2021-10-22 16:05:11 +0800

    Randomize images every load

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+//COMPONENTS
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+//STYLES
 import "../../styles/carousel.scss";
 
+//UTILS
+import {shuffle} from "../../utils/helpers.jsx"
+
 const CarouselControl = ({ items }) => {
+    //STATES
+    const [images, setImages] = useState(items)
+
+    //FUNCTIONS
+    useEffect(() => {
+       setImages(shuffle(images));
+    }, []);
+
     return (
         <Carousel showArrows={true}>
             {items.map( item => (
